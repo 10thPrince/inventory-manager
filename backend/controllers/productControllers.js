c1010029393f4c62236c05e7d49de91e2bae7d1e
10thPrince <ntwariprince5050@gmail.com>
2025-10-07 12:14:43 +0200

    Finished the product routing

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -12,23 +12,23 @@ export const getAllProducts = async (req, res) => {
             count: products.length,
             data: products
         })
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        res.status(500).json({message: "System Error!"})
+        res.status(500).json({ message: "System Error!" })
     }
-   
+
 }
 
 //@DESC create a product
 //@ROUTE POST /product/
 //@ACCESS Private
-export const createProduct = async(req, res) => {
+export const createProduct = async (req, res) => {
 
-    try{
-        const {name, unitPrice, quantity, lowStockAlert} = req.body;
+    try {
+        const { name, unitPrice, quantity, lowStockAlert } = req.body;
 
-        if(!name || !unitPrice){
-            return res.status(400).json({message: "Plaese Fill in required Fields!!"})
+        if (!name || !unitPrice) {
+            return res.status(400).json({ message: "Plaese Fill in required Fields!!" })
         }
 
         const newProduct = {
@@ -40,23 +40,67 @@ export const createProduct = async(req, res) => {
 
         const product = await Product.create(newProduct);
 
-        res.status(201).json({message: "Product created Successfull!", data: product})
-    }catch(err){
+        res.status(201).json({ message: "Product created Successfull!", data: product })
+    } catch (err) {
         console.log(err);
-        res.status(500).json({message: "System Error!"})
+        res.status(500).json({ message: "System Error!" })
     }
 }
 
 //@DESC update a product
 //@ROUTE PUT /product/:id
 //@ACCESS Private
-export const updateProduct = () => {
+export const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        const product = await Product.findById(id);
+
+        if(!product){
+            return res.status(404).json({message: "Invalid Id!"});
+        }
+
+        const {name, unitPrice, quantity, lowStockAlert} = req.body;
+
+        if(!name || !unitPrice){
+            return res.status(400).json({message: "Please fill In all required!"});
+        }
+
+        const updatedProduct = {
+            name,
+            unitPrice,
+            quantity,
+            lowStockAlert
+        }
+
+        const updateProduct = await Product.findByIdAndUpdate(id, updatedProduct);
+
+        res.status(202).json({message: "Product Updated Successful!", data: updateProduct});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "System Error!" });
+    }
 }
 
 //@DESC  delete a product
 //@ROUTE DELETE /product/:id
 //@ACCESS Private
-export const deleteProduct = () => {
+export const deleteProduct = async(req, res) => {
+    try {
+        const { id } = req.params;
 
+        const product = await Product.findById(id);
+
+        if(!product){
+            return res.status(404).json({message: "Invalid Id!"});
+        }
+
+        if(await Product.findByIdAndDelete(id)){
+            res.status(200).send("Book deleted successfully");
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'System Error!'})
+    }
 }
